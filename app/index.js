8a756b8149c6dbcfaa90c400167b640e161bf382
Vincent Kempers <vincent.m.kempers@gmail.com>
2016-09-14 22:28:09 +0200

    the whole thing

--- app/index.js
+++ app/index.js
@@ -3,20 +3,32 @@ var express = require('express');
 var app = express();
 const motivations = require('motivations');
 const pickOne = require('pick-one');
+var exphbs = require('express-handlebars');
+const catImages = [1,2,3,4,5,6];
+
+app.use(express.static('./app/public'));
 
 // 1.create a view engine 7-10
 app.engine('handlebars', exphbs({
+  defaultLayout: 'main',
+  layoutsDir:'./app/views/layouts'
+}));
 
-}))
+app.set('views', './app/views');
+app.set('view engine', 'handlebars');
 
-app.get('/', function(req, res) {
-  res.send(pickOne(motivations));
+
+
+app.get('/', function(request, response) {
+  var motivation = pickOne(motivations);
+  var image = pickOne(catImages);
+  response.render('motivation', { motivation, image });
 });
 
-app.get('/:motivationID', function(req, res) {
-  var id = req.params.motivationID;
-  res.send(motivations[id]);
-});
+// app.get('/:motivationID', function(req, res) {
+//   var id = req.params.motivationID;
+//   res.send(motivations[id]);
+// });
 
 
 
