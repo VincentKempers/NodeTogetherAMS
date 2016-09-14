9ab79e41d6e49d18bb3f415db840cc6f5ed5c145
Vincent Kempers <vincent.m.kempers@gmail.com>
2016-09-14 21:03:59 +0200

    add routes

--- server.js
+++ server.js
@@ -3,8 +3,13 @@ var express = require('express');
 var host = '127.0.0.1';
 var port = '8008';
 
+var routes = require('./app/index.js');
+
 var app = express();
 
+app.use(routes);
+
+// app.<action>(<route>, function(request,response) {})
 app.listen(port,host);
 
 
