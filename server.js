e86b95c84728d7a39990e50b9e32ce92e3b040bd
Vincent Kempers <vincent.m.kempers@gmail.com>
2016-09-14 20:27:09 +0200

    create server that listens at 127.0.0.1:8008

--- server.js
+++ server.js
@@ -1,2 +1,11 @@
 // it grabs the express code.
 var express = require('express');
+var host = '127.0.0.1';
+var port = '8008';
+
+var app = express();
+
+app.listen(port,host);
+
+
+console.log('Puur gestuurd');
